5197c95ad88ff4178211f853bd9479b1a268381a
Spencer Childress <samussiah@gmail.com>
2017-01-23 11:20:10 -0500

    Clear detail table on control change.

--- src/onResize.js
+++ src/onResize.js
@@ -3,13 +3,13 @@ import { dataOps } from 'webcharts';
 import addBoxplot from './util/addBoxplot';
 
 export default function onResize() {
-    const decim = format(".2f");
+    const decim = format('.2f');
     // Draw box plots
-    this.svg.selectAll("g.boxplot").remove()
+    this.svg.selectAll('g.boxplot').remove()
 
     // Y-axis box plot
     var yValues = this.current_data.map(function(d){return d.values.y}) 
-    var ybox = this.svg.append("g").attr("class","yMargin")
+    var ybox = this.svg.append('g').attr('class','yMargin')
     addBoxplot(
         ybox,
         yValues, 
@@ -17,14 +17,14 @@ export default function onResize() {
         1, 
         this.y_dom, 
         10, 
-        "#bbb", 
-        "white"
+        '#bbb', 
+        'white'
     )
-    ybox.select("g.boxplot").attr("transform", "translate(" + (this.plot_width + this.config.margin.right/2) + ",0)")
+    ybox.select('g.boxplot').attr('transform', 'translate(' + (this.plot_width + this.config.margin.right/2) + ',0)')
 
     //X-axis box plot
     var xValues = this.current_data.map(function(d){return d.values.x}) 
-    var xbox = this.svg.append("g").attr("class","xMargin")
+    var xbox = this.svg.append('g').attr('class','xMargin')
     addBoxplot(
         xbox, //svg element
         xValues,  //values
@@ -32,12 +32,12 @@ export default function onResize() {
         this.plot_width, //width
         this.x_dom, //domain
         10, //box plot width 
-        "#bbb", //box color
-        "white", //detail color
-        "0.2f", //format
+        '#bbb', //box color
+        'white', //detail color
+        '0.2f', //format
         false // horizontal?
     )
-    xbox.select("g.boxplot").attr("transform", "translate(0," + -(this.config.margin.top/2) + ")")
+    xbox.select('g.boxplot').attr('transform', 'translate(0,' + -(this.config.margin.top/2) + ')')
     
     //get list of visits
     var possibleVisits = set( this.allData.filter(f => f[this.config.measure_col] === this.config.measure).map(d => d[this.config.time_col]) )
@@ -53,16 +53,17 @@ export default function onResize() {
     this.y_dom[1] = this.y_dom[1] < 0 ?  this.y_dom[1]*0.99 : this.y_dom[1]*1.01;
 
     //reset view
-    this.svg.selectAll("g.point").classed("selected",false)
-        .select("circle").style("fill",this.config.colors[0]);
-
+    this.svg.selectAll('g.point').classed('selected',false)
+        .select('circle').style('fill',this.config.colors[0]);
+    this.wrap.select('.record-note').text('Click and drag to select points.');
+    this.detailTable.draw([]);
 
     //brushing
     function brushed(){
         var extent = brush.extent()
-        var points = this.svg.selectAll("g.point").classed("selected",false);
+        var points = this.svg.selectAll('g.point').classed('selected',false);
 
-        points.select("circle").attr("fill-opacity",0);
+        points.select('circle').attr('fill-opacity',0);
 
         var selected_points = points.filter(d => {
                 var cx = this.x(+d.values.x);
@@ -70,18 +71,18 @@ export default function onResize() {
                 return extent[0][0] <= cx && cx <= extent[1][0]
                     && extent[0][1] <= cy && cy <= extent[1][1];
             })
-            .classed("selected",true)
-            .select("circle").attr("fill-opacity", this.config.marks[0].attributes['fill-opacity'] );
+            .classed('selected',true)
+            .select('circle').attr('fill-opacity', this.config.marks[0].attributes['fill-opacity'] );
 
         //redraw the table with the new data
         var selected_data = selected_points.data().map(m => m.values.raw[0]);
         this.detailTable.draw(selected_data);
 
         //footnote
-        this.wrap.select('.record-note').text("Details of " + selected_data.length + " selected points:")
+        this.wrap.select('.record-note').text('Details of ' + selected_data.length + ' selected points:')
         if(brush.empty()){
-            this.wrap.select('.record-note').text("Click and drag to select points");
-            points.select("circle").attr("fill-opacity", this.config.marks[0].attributes['fill-opacity'] );
+            this.wrap.select('.record-note').text('Click and drag to select points');
+            points.select('circle').attr('fill-opacity', this.config.marks[0].attributes['fill-opacity'] );
         }
   
     };//brushed
@@ -89,11 +90,11 @@ export default function onResize() {
     var brush = svg.brush()
         .x(d3.scale.identity().domain(this.x.range()))
         .y(d3.scale.identity().domain(this.y.range()))
-        .on("brush", brushed.bind(this));
+        .on('brush', brushed.bind(this));
 
     this.svg.call(brush);    
 
-    this.svg.select("rect.extent")
+    this.svg.select('rect.extent')
         .attr({
             'shape-rendering': 'crispEdges',
             'stroke-width': 1,
@@ -113,15 +114,15 @@ export default function onResize() {
         chart.y.domain()[1]
     ])
 
-    this.svg.select("line.identity").remove()
-    this.svg.append("line")
-    .attr("x1",chart.x(overallMin))
-    .attr("x2",chart.x(overallMax))
-    .attr("y1",chart.y(overallMin))
-    .attr("y2",chart.y(overallMax))    
-    .attr("stroke","black")
-    .attr("clip-path","URL(#1)")
-    .attr("class","identity")
+    this.svg.select('line.identity').remove()
+    this.svg.append('line')
+    .attr('x1',chart.x(overallMin))
+    .attr('x2',chart.x(overallMax))
+    .attr('y1',chart.y(overallMin))
+    .attr('y2',chart.y(overallMax))    
+    .attr('stroke','black')
+    .attr('clip-path','URL(#1)')
+    .attr('class','identity')
 
   //Add tooltip to axis labels listing selected visits.
     this.svg.selectAll('.x.axis .axis-title')
